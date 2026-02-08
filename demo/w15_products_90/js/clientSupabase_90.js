3126e50fd787e807f19e1156d745f9f0843b0ef8
Sky00l <207410290@gms.tku.edu.tw>
2026-02-08 18:56:44 +0800

    update supabase url

--- demo/w15_products_90/js/clientSupabase_90.js
+++ demo/w15_products_90/js/clientSupabase_90.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://ophlhndtmgrbqpyaatag.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waGxobmR0bWdyYnFweWFhdGFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0NTkzNDcsImV4cCI6MTk5MjAzNTM0N30.ZuXrcaVq24uc7FVKJ-pHR3ViZ5-FwPRtUMziGxayW7Y';
+const SUPABASE_URL = 'https://svqlehbiijfgqvrgqplx.supabase.cotps://ophlhndtmgrbqpyaatag.supabase.co';
+const SUPABASE_ANON_KEY = 'VITE_SUPABASE_ANON_KEY = sb_publishable_ZhDpMmKSAEtmjBOyDtLXfg_rihLcXo-';
 
 export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
