b0869d81b09fcdbff1cb4d62b9eea3ae78a5794b
Sky00l <207410290@gms.tku.edu.tw>
2026-02-08 18:58:49 +0800

    fix supabase url

--- demo/w15_products_90/js/clientSupabase_90.js
+++ demo/w15_products_90/js/clientSupabase_90.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://svqlehbiijfgqvrgqplx.supabase.cotps://ophlhndtmgrbqpyaatag.supabase.co';
-const SUPABASE_ANON_KEY = 'VITE_SUPABASE_ANON_KEY = sb_publishable_ZhDpMmKSAEtmjBOyDtLXfg_rihLcXo-';
+const SUPABASE_ANON_KEY = 'sb_publishable_ZhDpMmKSAEtmjBOyDtLXfg_rihLcXo-';
 
 export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
